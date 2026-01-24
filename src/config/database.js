8bc73384236a7f532c6a1744e1241928e4adb985
anjardev <sanjaya.ginanjar@gmail.com>
2026-01-24 08:28:25 +0700

    Refactor database configuration to remove unnecessary comment

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const databaseConfig = {
-  dialect: 'mysql', // HARUS STRING
+  dialect: 'mysql',
   host: process.env.DB_HOST || '127.0.0.1',
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
